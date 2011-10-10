a606132628ecc14e2c576699f54ac428f0e55a18
kevin.verschaete <kevin.verschaete@gmail.com>
2011-10-10 13:06:40 +0000

    Cration classe GenerationNombres

---
+++ generationNombresAleatoires/src/generateur/GenerationNombres.java
@@ -0,0 +1,10 @@
+package generateur;
+
+/**
+ * Classe qui permet de générer des nombres pseudo aléatoire suivant 5 lois différentes
+ * @author Kévin
+ *
+ */
+public class GenerationNombres {
+
+}
