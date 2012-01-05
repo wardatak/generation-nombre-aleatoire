ebf3336471a1c984f18b4523617470ee2932401d
projet.avcsg <projet.avcsg@2009f4ab-ef24-8f0f-263e-4c9651209126>
2012-01-05 16:13:15 +0000

    Ajout d'un IHM de test

--- trunk/generationNombresAleatoires/src/gestionTraffic/Carrefour.java
+++ trunk/generationNombresAleatoires/src/gestionTraffic/Carrefour.java
@@ -1,34 +1,45 @@
 package gestionTraffic;
 
+import generateur.GenerateurExponentielle;
+import generateur.PositiveGaussian;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.Timer;
 
-import generateur.GenerateurNormale;
-import generateur.PositiveGaussian;
-
 public class Carrefour {
 	
-	/** Feux Verticaux */
+	/* Feux Verticaux */
 	Feu feuV1, feuV2;
-	/** Feux Horizontaux */
+	/* Feux Horizontaux */
 	Feu feuH1, feuH2;
-	/** lambda de la route horizontale*/
+	/* lambda de la route horizontale*/
 	int lambdaHorizontal = 0;
-	/** lambda de la route verticale*/
+	/* lambda de la route verticale*/
 	int lambdaVertical = 0;
-	/** Temps d'attente des feux verts de la route horizontale*/
+	/* Temps d'attente des feux verts de la route horizontale*/
 	double tempsDattHorizontal = 0;
-	/** Temps d'attente des feux verts de la route verticale*/
+	/* Temps d'attente des feux verts de la route verticale*/
 	double tempsDattVertical = 0;
-	/** paramètres de la loi normale pour la route V */
+	/* paramètres de la loi normale pour la route V */
 	double moyenneV, ecartTypeV = 0;
-	/** paramètres de la loi normale pour la route H */
+	/* paramètres de la loi normale pour la route H */
 	double moyenneH, ecartTypeH = 0;
-	/** timer pour les temps d'attente */
-	private Timer timerFeux;
-	private Timer timerFeux2;
+	/* timer pour les temps d'attente */
+	private Timer timerFeuxVertV;
+	private Timer timerFeuxVertH;
+	/* timer pour l'arrivée des voitures sur chaque feu */
+	private Timer timerTraficV1;
+	private Timer timerTraficV2;
+	private Timer timerTraficH1;
+	private Timer timerTraficH2;
+	/* timer pour le départ des voitures */
+	private Timer timerDepartH;
+	private Timer timerDepartV;
+	int tempsDepart = 1000;
+	int baseDeTemps = 1000;
+	private double trafficV1, trafficV2, trafficH1, trafficH2;
 	
 	public Carrefour(){
 		Feu feuRouge = new Feu(0);
@@ -37,67 +48,173 @@ public class Carrefour {
 		feuV2 = feuRouge;
 		feuH1 = feuVert;
 		feuH2 = feuVert;
-		moyenneV = 6;
-		ecartTypeV = 1;
-		moyenneH = 10;
+		moyenneV = 20;
+		ecartTypeV = 2;
+		moyenneH = 40;
 		ecartTypeH = 2;
+		trafficV1 = 0.5d;
+		trafficV2 = 0.5d;
+		trafficH1 = 1d;
+		trafficH2 = 1d;
+		timerDepartV = new Timer(tempsDepart , new ActionListener() {
+					
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				feuV1.removeVoitureFileCourante();
+				feuV2.removeVoitureFileCourante();
+			}
+		});
+		
+		timerDepartH = new Timer(tempsDepart , new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				feuH1.removeVoitureFileCourante();
+				feuH2.removeVoitureFileCourante();
+			}
+		});
 		
 	}
 
 	public void run(){
 		
-		attenteV(2000);
+		attenteV(200);
+		trafficV1(feuV1);
+		trafficV2(feuV2);
+		trafficH1(feuH1);
+		trafficH2(feuH2);
 		while(true);
 			
 	}
 	
+	public void trafficV1(Feu f){
+		GenerateurExponentielle genExponentielle = new GenerateurExponentielle();
+		double t = genExponentielle.generationAleatoire(trafficV1);
+		final Feu fs = f;
+		Integer i = (int) Math.round(t*baseDeTemps);
+		timerTraficV1 = new Timer(i , new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				fs.addVoitureFileCourante("FeuV1");
+				ihm.IHMTestTimer.changeLabelTrafficV1(fs.getFileCourante());
+				timerTraficV1.stop();
+				trafficV1(fs);
+			}
+			
+		});
+		timerTraficV1.start();
+	}
+	
+	public void trafficV2(Feu f){
+		GenerateurExponentielle genExponentielle = new GenerateurExponentielle();
+		double t = genExponentielle.generationAleatoire(trafficV2);
+		final Feu fs = f;
+		Integer i = (int) Math.round(t*baseDeTemps);
+		timerTraficV2 = new Timer(i , new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				fs.addVoitureFileCourante("FeuV2");
+				ihm.IHMTestTimer.changeLabelTrafficV2(fs.getFileCourante());
+				timerTraficV2.stop();
+				trafficV2(fs);
+			}
+			
+		});
+		timerTraficV2.start();
+	}
+	
+	public void trafficH1(Feu f){
+		GenerateurExponentielle genExponentielle = new GenerateurExponentielle();
+		double t = genExponentielle.generationAleatoire(trafficH1);
+		final Feu fs = f;
+		Integer i = (int) Math.round(t*baseDeTemps);
+		timerTraficH1 = new Timer(i , new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				fs.addVoitureFileCourante("FeuH1");
+				ihm.IHMTestTimer.changeLabelTrafficH1(fs.getFileCourante());
+				timerTraficH1.stop();
+				trafficH1(fs);
+			}
+			
+		});
+		timerTraficH1.start();
+	}
+	
+	public void trafficH2(Feu f){
+		GenerateurExponentielle genExponentielle = new GenerateurExponentielle();
+		double t = genExponentielle.generationAleatoire(trafficH2);
+		final Feu fs = f;
+		Integer i = (int) Math.round(t*baseDeTemps);
+		timerTraficH2 = new Timer(i , new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				fs.addVoitureFileCourante("FeuH2");
+				ihm.IHMTestTimer.changeLabelTrafficH2(fs.getFileCourante());
+				timerTraficH2.stop();
+				trafficH2(fs);
+			}
+			
+		});
+		timerTraficH2.start();
+	}
+	
 	public void attenteV(int i){
-		timerFeux = new Timer(i , new ActionListener() {
+		timerFeuxVertV = new Timer(i , new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				PositiveGaussian n = new PositiveGaussian(moyenneV, ecartTypeV); // Temps du feu vert d'après la loi normale
 				tempsDattVertical = n.getRandomValue();
 				//wait(tempsDattHorizontal);
-				Integer i = (int) Math.round(tempsDattVertical*100);
+				Integer i = (int) Math.round(tempsDattVertical*baseDeTemps);
 				routeVerticaleAuVert();
 				attenteH(i);
-				timerFeux.stop();
+				timerFeuxVertV.stop();
 			}
 		});
-		timerFeux.start();
+		timerFeuxVertV.start();
 	}
 	
 	public void attenteH(int i){
-		timerFeux2 = new Timer(i , new ActionListener() {
+		timerFeuxVertH = new Timer(i , new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				PositiveGaussian n = new PositiveGaussian(moyenneH, ecartTypeH); // Temps du feu vert d'après la loi normale
 				tempsDattHorizontal = n.getRandomValue();
 				//wait(tempsDattHorizontal);
-				Integer i = (int) Math.round(tempsDattHorizontal*100);
+				Integer i = (int) Math.round(tempsDattHorizontal*baseDeTemps);
 				routeHorizontaleAuVert();
 				attenteV(i);
-				timerFeux2.stop();
+				timerFeuxVertH.stop();
 			}
 		});
-		timerFeux2.start();
+		timerFeuxVertH.start();
 	}
 	
 	public void routeVerticaleAuVert(){
-		feuV1.passerAuVert();
-		feuV2.passerAuVert();
-		feuH1.passerAuRouge();
-		feuH2.passerAuRouge();
+		timerDepartV.stop();
+		timerDepartH.start();
+		
+		feuV1.passerAuVert("FeuV1");
+		feuV2.passerAuVert("FeuV2");
+		feuH1.passerAuRouge("FeuH1");
+		feuH2.passerAuRouge("FeuH2");
 		System.out.println("Route verticale verte / Route horizontale rouge");
 	}
 	
 	public void routeHorizontaleAuVert(){
-		feuV1.passerAuRouge();
-		feuV2.passerAuRouge();
-		feuH1.passerAuVert();
-		feuH2.passerAuVert();
+		timerDepartH.stop();
+		timerDepartV.start();
+		feuV1.passerAuRouge("FeuV1");
+		feuV2.passerAuRouge("FeuV2");
+		feuH1.passerAuVert("FeuH1");
+		feuH2.passerAuVert("FeuH2");
 		System.out.println("Route verticale rouge / Route horizontale verte");
 	}
 	
--- trunk/generationNombresAleatoires/src/gestionTraffic/Feu.java
+++ trunk/generationNombresAleatoires/src/gestionTraffic/Feu.java
@@ -10,22 +10,36 @@ public class Feu {
 		fileCourante = 0;
 	}
 
-	public void passerAuRouge() {
+	public void passerAuRouge(String feu) {
 		this.couleur = 0;
 		//Paint component
+		ihm.IHMTestTimer.changeBackgroundRouge(feu);
 	}
 
-	public void passerAuVert() {
+	public void passerAuVert(String feu) {
 		this.couleur = 1;
 		//Paint component
+		ihm.IHMTestTimer.changeBackgroundVert(feu);
 	}
 
 	public int getFileCourante() {
 		return fileCourante;
 	}
 
-	public void setFileCourante(int fileCourante) {
-		this.fileCourante = fileCourante;
+	public void addVoitureFileCourante(String feu) {
+		this.fileCourante++;
+		System.out.println("\tarrivée voiture file");
+		//ihm.IHMTestTimer.changeLabelTraffic(feu, this.fileCourante);
+		
+	}
+	
+	public void removeVoitureFileCourante() {
+		System.out.println("\t\t\t\t\t\t\t\tdepart voiture file");
+		if(this.fileCourante > 0){
+			this.fileCourante--;
+		}else{
+			this.fileCourante = 0;
+		}
 	}
 	
 	public boolean isRed(){
